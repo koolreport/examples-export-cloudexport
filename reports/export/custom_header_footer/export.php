0cf26fa72c96d6884e83569bc78034d957f5fb77
dongnl <nguyenluongdong@gmail.com>
2025-04-23 16:14:58 +0700

    Init code

---
+++ reports/export/custom_header_footer/export.php
@@ -0,0 +1,21 @@
+<?php
+ob_start();
+require_once "SakilaRental.php";
+$report = new SakilaRental;
+ob_end_clean();
+$report->run()
+->export('SakilaRentalPDF')
+->pdf(array(
+    "headerCallback" => "function(headerContent, pageNum, numPages){
+        if (pageNum == 1) return ''; //don't show header for the 1st page
+        return headerContent;
+    }",
+    "footerCallback" => "function(footerContent, pageNum, numPages){
+        if (pageNum == 1) return ''; //don't show footer for the 1st page
+        return footerContent.replace('{pageNum}', pageNum - 1);
+    }",
+    "format"=>"A4",
+    "orientation"=>"portrait",
+    //"zoom"=>2
+))
+->toBrowser("SakilaRentalPDF.pdf");
---
+++ reports/export/sakila_rental/export.php
@@ -0,0 +1,14 @@
+<?php
+ob_start();
+
+require_once "SakilaRental.php";
+$report = new SakilaRental;
+ob_end_clean();
+$report->run()
+->export('SakilaRentalPdf')
+->pdf(array(
+    "format"=>"A4",
+    "orientation"=>"portrait",
+    //"zoom"=>2
+))
+->toBrowser("sakila_rental.pdf");
